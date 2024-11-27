22ca9fd1c22f804e35f1ce3034bcbe8b900ad2df
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-27 12:20:54 -0300

    ADmin

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .routes.home import home
 from .routes.login import login
 from .routes.refeicoes import refeicoes
+from .routes.admin import admin
 from .routes.api import api 
 import os
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(home)
 app.register_blueprint(login, url_prefix='/login')
 app.register_blueprint(refeicoes, url_prefix='/refeicoes')
+app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(api, url_prefix='/api')
 
 app.secret_key = os.environ.get('SECRET_KEY')
