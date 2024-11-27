993fb0f95521dd3075e5a02c3f9bcb9e9f7f1255
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-27 12:48:18 -0300

    Login

---
+++ static/js/login.js
@@ -0,0 +1,40 @@
+document.querySelector('#botao-login').addEventListener('click', async () => {
+    let matricula = document.getElementsByName('matricula')[0].value; 
+    let senha = document.getElementsByName('senha')[0].value; 
+
+    if (matricula == '' || senha == '') {
+        Swal.fire({
+            icon: "error",
+            title: "Campos vazios",
+            text: "Preencha os campos corretamente."
+        });
+
+        return;
+    }
+
+    let resposta = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'matricula': matricula,
+            'senha': senha
+        })
+    });
+
+    let respostaJSON = await resposta.json();
+
+    if (!respostaJSON.ok) {
+        Swal.fire({
+            icon: "error",
+            title: "Erro",
+            text: respostaJSON.mensagem
+        });
+        return;
+    }
+
+    
+    window.location.href = '/';
+});
