ef9bb492a0833342931528aea69d9ade9bf80848
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-27 00:58:01 -0300

    Definição de rotas

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from .routes.home import home
+from .routes.login import login
+from .routes.api import api 
+import os
 
 app = Flask(__name__)
+
+app.register_blueprint(home)
+app.register_blueprint(login, url_prefix='/login')
+app.register_blueprint(api, url_prefix='/api')
+
+app.secret_key = os.environ.get('SECRET_KEY')
