fcaa81891c37ce742dee20aec89668e2cb7b06d9
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-27 10:54:31 -0300

    Cadastro de itens no cardápio

--- routes/cardapio.py
+++ routes/cardapio.py
@@ -47,3 +47,45 @@ def buscar_cardapios(data, tipo):
         cardapio_semana[dia] = itens_dia
     
     return jsonify({'ok': True, 'resultado': cardapio_semana}), 200
+
+@cardapio.route('/', methods=['POST'])
+def cadastrar_cardapio():
+    dados = request.json
+
+    parametros = {
+        'dia': lambda x: isinstance(x, str),
+        'tipo': lambda x: isinstance(x, str) and x in ['cafe', 'almoco', 'lanche', 'janta'],
+        'itens': lambda x: isinstance(x, list) and x
+    }
+
+    for parametro in parametros:
+        if parametro not in dados:
+            return jsonify({'ok': False, 'mensagem': 'Parâmetro obrigatório não informado.'}), 400
+        argumento = dados[parametro]
+        if not parametros[parametro](argumento):
+            return jsonify({'ok': False, 'mensagem': 'Argumento em formato inválido.'}), 400
+    
+    lista_itens = dados['itens']
+
+    for item in lista_itens:
+        try:
+            db.query('INSERT INTO itens_cardapios_dias VALUES (%s, %s, %s);',
+                dados['dia'],
+                dados['tipo'],
+                item
+            )
+        except:
+            return jsonify({'ok': False, 'mensagem': 'Erro ao tentar cadastrar os itens.'}), 400
+    
+    return jsonify({'ok': True, 'mensagem': 'Itens cadastrados.'}), 200
+
+@cardapio.route('/', methods=['DELETE'])
+def deletar_cardapio():
+    dados = request.json
+
+    parametros = {
+        'dia': lambda x: isinstance(x, str),
+        'tipo': lambda x: isinstance(x, str) and x in ['cafe', 'almoco', 'lanche', 'janta'],
+        'item': lambda x: isinstance(x, str)
+    }
+
